3bdab6002a9b8a76df3cbd61b4b941a4789d79fe
KP Web Dev <kpwebdevishere@gmail.com>
2023-10-19 17:55:39 +0530

    added animation to the hero section of the nfc page

--- src/components/nfc/nfcHero.component.jsx
+++ src/components/nfc/nfcHero.component.jsx
@@ -2,6 +2,12 @@ import carouselImg1 from "../../assets/carousel-img-1.png";
 import carouselImg2 from "../../assets/carousel-img-2.png";
 import carouselImg3 from "../../assets/carousel-img-3.png";
 
+// lottie animation
+import Lottie from "lottie-react";
+import emilyAnimation from "../../assets/lotties/emily-animation.json";
+import cardSlideAnimation from "../../assets/lotties/card-slide-animation.json";
+import { Link } from "react-router-dom";
+
 const NfcHero = () => {
   return (
     <div
@@ -32,26 +38,69 @@ const NfcHero = () => {
         ></button>
       </div>
       <div className="carousel-inner">
-        <div className="carousel-item active">
-          <img
-            src={carouselImg1}
-            className="d-block w-100"
-            alt="Carousel Image 1"
-          />
+        <div
+          className="carousel-item active"
+          style={{
+            background: `linear-gradient(rgba(0,0,0,.60), rgba(0,0,0,.60)), url(${carouselImg2})`,
+            backgroundSize: "cover",
+          }}
+        >
+          {/* container */}
+          <div className="container t-grid t-items-center t-grid-cols-2 t-min-h-screen">
+            {/* text container */}
+            <div className="t-text-f-primary-99">
+              <h2
+                className="t-text-f-4xl t-mb-f-24"
+                style={{ textShadow: "0px 8px 8px rgba(0,0,0,.5)" }}
+              >
+                TapThe future of Networking in a single{" "}
+                <span className="t-text-f-secondary-50">Tap</span>
+              </h2>
+              <Link className="f-btn-lg f-btn-primary" to="#get-started">
+                Get Started
+              </Link>
+            </div>
+            {/* animation container */}
+            <div>
+              <Lottie
+                animationData={emilyAnimation}
+                className="t-scale-150"
+                loop={true}
+              />
+            </div>
+          </div>
         </div>
-        <div className="carousel-item">
-          <img
-            src={carouselImg2}
-            className="d-block w-100"
-            alt="Carousel Image 2"
-          />
-        </div>
-        <div className="carousel-item">
-          <img
-            src={carouselImg3}
-            className="d-block w-100"
-            alt="Carousel Image 3"
-          />
+        <div
+          className="carousel-item active"
+          style={{
+            background: `linear-gradient(rgba(0,0,0,.60), rgba(0,0,0,.60)), url(${carouselImg3})`,
+            backgroundSize: "cover",
+          }}
+        >
+          {/* container */}
+          <div className="container t-grid t-items-center t-grid-cols-2 t-min-h-screen">
+            {/* text container */}
+            <div className="t-text-f-primary-99">
+              <h2
+                className="t-text-f-4xl t-mb-f-24"
+                style={{ textShadow: "0px 8px 8px rgba(0,0,0,.5)" }}
+              >
+                Discover our variety of inbuilt templates to make your work{" "}
+                <span className="t-text-f-secondary-50">easier</span>
+              </h2>
+              <Link className="f-btn-lg f-btn-primary" to="browse-templates">
+                Browse Templates
+              </Link>
+            </div>
+            {/* animation container */}
+            <div>
+              <Lottie
+                animationData={cardSlideAnimation}
+                className="t-scale-150"
+                loop={true}
+              />
+            </div>
+          </div>
         </div>
       </div>
       <button
